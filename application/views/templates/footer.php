f825b35e65a60c14c3ecedf3c80d85e38722a6d8
emptywork <22065214+EmptyWork@users.noreply.github.com>
2021-11-30 18:04:01 +0900

    Update style.css and footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,8 @@
-  <footer>
-    <em>Copyright &copy; 2021 - G. Stevarth Hoke</em>
+  <footer class="footer">
+    <div class="footer-under">
+      <span class="footer-under-info">Dibuat oleh <a href="https://emptywork.netlify.app" target="_blank" rel="noopener noreferrer">Stevarth</a></span>
+      <span class="footer-under-info">Powered by <a href="https://codeigniter.com" target="_blank" rel="noopener noreferrer">CodeIgniter3</a></span>
+    </div>
   </footer>
 </body>
 </html>
