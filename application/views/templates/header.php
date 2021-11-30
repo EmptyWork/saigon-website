6caf8be503773ac1e686889e17c1425debd24ac9
emptywork <22065214+EmptyWork@users.noreply.github.com>
2021-12-01 08:32:52 +0900

    Update the favicon.svg path

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $title ?> - Sai Gon in The Rain (2020)</title>
-  <link rel="shortcut icon" href="<?= base_url(); ?>favicon.svg" type="image/x-svg">
+  <link rel="shortcut icon" href="<?= base_url(); ?>images/favicon.svg" type="image/x-svg">
   <link rel="stylesheet" href="<?= base_url(); ?>styles/style.css">
   <script src="<?= base_url(); ?>js/script.js" defer></script>
 </head>
