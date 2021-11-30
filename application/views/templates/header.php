3998c2b1aa35c0251598d3d99f7c8eeab6c28afb
emptywork <22065214+EmptyWork@users.noreply.github.com>
2021-11-30 17:46:28 +0900

    Update the title and add favicon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,8 +5,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $title ?> | G. Stevarth Hoke</title>
-  
+  <title><?= $title ?> - Sai Gon in The Rain (2020)</title>
+  <link rel="shortcut icon" href="favicon.svg" type="image/x-svg">
   <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
