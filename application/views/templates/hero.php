b5bc315edc17232d7325ed4ea83235265817adfa
emptywork <22065214+EmptyWork@users.noreply.github.com>
2021-11-30 17:46:03 +0900

    Update style.css and hero.php

--- application/views/templates/hero.php
+++ application/views/templates/hero.php
@@ -1,3 +1,13 @@
-<header>
-  <h1>Hero</h1>
+<header class="header">
+  <div class="hero">
+    <div class="hero-text">
+      <h1>Sai Gon Trong Con Mua</h1>
+      <p>Sai Gon in The Rain <span class="year">(2020)</span></p>
+      <p>Under the drizzling rain of the bustling Saigon city, a dreamy musician and a hard-headed realistic girl stumble across each other and fall in love.</p>
+      <button class="hero-action">
+        <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+        Watch
+      </button>
+    </div>
+  </div>
 </header>
