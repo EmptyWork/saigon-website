adea64569b889ad4d8507826e026a0e747de2c45
emptywork <22065214+EmptyWork@users.noreply.github.com>
2021-12-01 20:06:55 +0900

    Create review quote and its styling

--- application/views/pages/review.php
+++ application/views/pages/review.php
@@ -1,3 +1,5 @@
 <main class="main">
   <h1><?= $title ?></h1>
+  <q class="kutipan">Sebuah <span class="highlight">cerita cinta klasis tentang dua orang manusia yang tidak sempurna</span>, yang melukai diri mereka sendiri; Dibalik ilusi kota yang mematikan karakter.</q>
+  <div>Stevarth</div>
 </main>
